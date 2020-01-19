b125170ed132c0ef3e584ff8219ee2887626eee7
Administrator <Administrator@192.168.1.105>
2020-01-19 13:20:16 +0800

    system

--- System/src/main/java/com/wwy/SystemApplication.java
+++ System/src/main/java/com/wwy/SystemApplication.java
@@ -2,7 +2,11 @@ package com.wwy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 
+ * @author wwy
+ *
+ */
 @SpringBootApplication
 public class SystemApplication {
 
